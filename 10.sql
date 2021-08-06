2bfba9b18a1cd561d25a35bd0a189be49ef042c5
Elmars Vanags <vanags.el@gmail.com>
2021-08-06 21:57:20 +0300

    first commit

--- 10.sql
+++ 10.sql
@@ -5,6 +5,6 @@
 It gives less results per student (no more course Java is available), because relation with course_id 2 (Java) doesn't exists anymore.
 For example, query number 9 then gives course count -1. (If there was 2 courses, then after deletion it will be only 1).
 
-But query number 8 gives no Java as result anymore. */
+But query number 8 gives no Java as result anymore.. */
 
 DELETE FROM student_course WHERE course_id='2';
